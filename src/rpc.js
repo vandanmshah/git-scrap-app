2d70fd9bb55c66cdf812ea51387aca32d256ade4
Vandan Shah <vandanshah77@gmail.com>
2018-10-17 11:55:05 +0530

    [IMP] commment rpc.js file and  also add readme.md file

--- src/rpc.js
+++ src/rpc.js
@@ -1,17 +1,31 @@
+/**
+ * @public
+ * 
+ * @param {*} params.method mehtod name deined in python which we want to call
+ */
 var _rpc = function (params) {
     return new _rpc.rpc(params);
 }
 _rpc.rpc = function (params) {
     var self = this;
+    // Add loading spinner while calling python method
+
     var loadingSpinner = document.createElement('div');
     loadingSpinner.className = "loading_spinner";
     loadingSpinner.innerHTML = "Fetching data"
     document.body.appendChild(loadingSpinner);
+
     var promise = new Promise(function (resolve, reject) {
         self[params.method](params, resolve);
     });
     return promise;
 }
+/**
+ * @private
+ * all method name should be same as python's method name
+ * just decelare post method and pass your data
+ * 
+ */
 _rpc.prototype = {
     scrap_git_Data : function (params, resolve) {
         var postUrl = "http://" + window.location.hostname + ":5000/" + params.method;
